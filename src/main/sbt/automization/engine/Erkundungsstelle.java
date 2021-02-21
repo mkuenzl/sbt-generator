1cca14d4debdee954e3330b801943f3ade0f2b24
mkuenzl <33060049+mkuenzl@users.noreply.github.com>
2021-02-21 17:54:14 +0100

    removed abstract classes

--- src/main/sbt/automization/engine/Erkundungsstelle.java
+++ src/main/sbt/automization/engine/Erkundungsstelle.java
@@ -2,9 +2,62 @@ package sbt.automization.engine;
 
 import java.util.*;
 
-public class Erkundungsstelle extends AErkundungsstelle
+public class Erkundungsstelle implements Comparable<Erkundungsstelle>, IProjektData
 {
-    public Erkundungsstelle(Map<String,String> data) {
-        super(data);
+    private final Map<String,String> dataMap;
+    private List<Schicht> schichtList = new ArrayList<>();
+
+    Erkundungsstelle(Map<String,String> data) {
+
+        this.dataMap = new HashMap();
+
+        for (String key : data.keySet())
+        {
+            if (key.contains("ERK_")) dataMap.put(key, data.get(key));
+        }
+    }
+
+    public String getInformation(String key){
+        return dataMap.get(key);
+    }
+
+    @Override
+    public int compareTo(final Erkundungsstelle o)
+    {
+        return this.getInformation("ERK_ID").compareTo(o.getInformation("ERK_ID"));
+    }
+
+    void addSchicht(final Map<String,String> data)
+    {
+        Map<String, String> tmpMap = new HashMap<>();
+        for (String key : data.keySet())
+        {
+            if (key.contains("SCHICHT_") || key.contains("CHEMIE_")){
+                tmpMap.put(key, data.get(key));
+            }
+        }
+        schichtList.add(new Schicht(tmpMap));
+    }
+
+    public List<Schicht> getSchichtList()
+    {
+        return schichtList;
+    }
+
+    public List<Schicht> getSchichtAufschluss(final String aufschluss) {
+        List<Schicht> schichtAufschlussList = new ArrayList<>();
+        for (Schicht schicht :
+                schichtList)
+        {
+            if (aufschluss.equals(schicht.getInformation("SCHICHT_AUFSCHLUSS")))
+            {
+                schichtAufschlussList.add(schicht);
+            }
+        }
+        return schichtAufschlussList;
+    }
+
+    void sort(){
+        Collections.sort(schichtList);
     }
 }
--- src/main/sbt/automization/engine/Schicht.java
+++ src/main/sbt/automization/engine/Schicht.java
@@ -2,9 +2,37 @@ package sbt.automization.engine;
 
 import java.util.Map;
 
-public class Schicht extends ASchicht
+public class Schicht implements Comparable<Schicht>, IProjektData, Cloneable
 {
-    public Schicht(Map<String, String> information) {
-        super(information);
+    private final Map<String, String> dataMap;
+
+    Schicht(Map<String, String> dataMap) {
+        this.dataMap = dataMap;
+    }
+
+    // Schicht Parameter
+    public String getInformation(String key){
+        return dataMap.get(key);
+    }
+
+    public void setInformation(String key, String value){
+        this.dataMap.put(key,value);
+    }
+
+    // Chemie Parameter
+
+    @Override
+    public int compareTo(final Schicht schicht)
+    {
+        int s1 = Integer.parseInt(this.getInformation("SCHICHT_NR"));
+        int s2 = Integer.parseInt(schicht.getInformation("SCHICHT_NR"));
+        return s1 - s2;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException
+    {
+        Schicht cloned = (Schicht)super.clone();
+        return cloned;
     }
 }
