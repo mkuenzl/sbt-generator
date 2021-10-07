9d546fd78deb879529e329a3895393463210fef7
Moritz Kuenzl <s4mokuen@uni-trier.de>
2021-10-07 11:22:58 +0200

    Update TextFormatLPTest.java

--- src/tests/sbt/automization/format/TextFormatLPTest.java
+++ src/tests/sbt/automization/format/TextFormatLPTest.java
@@ -19,18 +19,25 @@ public class TextFormatLPTest
     public void formatEmptyEv85Test() throws IOException {
         String formattedCode = new LoadPlateTextFormatter().format("< 80", "-");
 
-        Assert.assertEquals(formattedCode, "<p class=Normal >\n" +
-                "< 80\n" +
-                "</p>");
+        Assert.assertEquals("< 80", formattedCode);
     }
 
     @Test
     public void formatEmptyTest() throws IOException {
         String formattedCode = new LoadPlateTextFormatter().format("-", "-");
 
-        Assert.assertEquals(formattedCode, "<p class=Normal >\n" +
-                "-\n" +
-                "</p>");
+        Assert.assertEquals(formattedCode, "-");
     }
 
+    @Test
+    public void formatEv2Ev85Test() throws IOException {
+        String formattedCode = new LoadPlateTextFormatter().format("< 80", "31");
+
+        Assert.assertEquals("<p class=Normal >" +
+                "\n< 80" +
+                "\n<p class=Normal6 >" +
+                "\n[50 - 60]" +
+                "\n</p>" +
+                "\n</p>", formattedCode);
+    }
 }
