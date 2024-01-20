c95ccd51e0426fa5fa64a5aee55690399f8743ad
Vincent Görlich <goerlichvincent@yahoo.de>
2024-01-20 10:25:22 +0100

    Code Cleanup

--- src/main/sbt/automization/core/retrieval/CompressibilityRetrieval.java
+++ src/main/sbt/automization/core/retrieval/CompressibilityRetrieval.java
@@ -11,17 +11,6 @@ public class CompressibilityRetrieval extends DatatableInformationRetrieval
 	{
 		super(SampleKey.COMPRESSIBILITY);
 	}
-
-//	@Override
-//	HtmlRow createRow()
-//	{
-//		HtmlRow row = HtmlFactory.createRow(styleParameter.getRowClass(), new HtmlCell[]{
-//				HtmlFactory.createCell(styleParameter.getHeaderCellClass(), styleParameter.getHeaderCellWidth(),
-//						new String[]{"Verdichtungsfähigkeit"})
-//		});
-//
-//		return row;
-//	}
 	
 	@Override
 	String retrieveFrom(Sample sample)
@@ -32,8 +21,7 @@ public class CompressibilityRetrieval extends DatatableInformationRetrieval
 	@Override
 	String retrieveFrom(Probe probe)
 	{
-		String information = new SamplePrinter().printAttributeOfSamplesWithDepth(probe, outcrop.toString(), informationKey);
-		
-		return information;
+
+		return new SamplePrinter().printAttributeOfSamplesWithDepth(probe, outcrop.toString(), informationKey);
 	}
 }
